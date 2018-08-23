b6a8ac34a29609f26ea91892915a0a32b67a2a36
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2018-08-23 18:07:31 +0530

    Multiple bugs and features added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import datetime
 
@@ -10,6 +11,7 @@ MSG_CONTENT = 0
 MSG_CH = 1
 MSG_CREATOR = 2
 MSG_REACTS = 3
+MSG_LINK = 3
 
 
 class OnThisDay:
@@ -27,13 +29,23 @@ class OnThisDay:
         # print(self.channel_list)
         self.messages_ch_list = self.list_messages("year")
         if len(self.messages_ch_list) == 0:
-            self.message = self.list_messages("month")
+            self.messages_ch_list = self.list_messages("month")
 
         if len(self.messages_ch_list) > 0:
             self.message = self.messages_ch_list[self.max_emoji_msg()]
+            self.decorated_message = self.decorate_msg()
             self.post_msg()
         else:
-            print("You guys should be talking more!")
+            msg = "DAMN! You guys should be talking more..."
+            channel_id = 'random'
+            for ch in self.channel_list:
+                if ch[CH_NAME] == 'random':
+                    channel_id = ch[CH_ID]
+                    break
+            self.client.api_call(method="chat.postMessage",
+                                 channel=channel_id,
+                                 text=msg
+                                 )
 
     def list_channels(self):
         """
@@ -46,8 +58,12 @@ class OnThisDay:
         """
         ch_list = []
         ch_dict = self.client.api_call(method="channels.list")
-        for ch in ch_dict["channels"]:
-            ch_list.append((ch["name"], ch["id"]))
+        try:
+            for ch in ch_dict["channels"]:
+                ch_list.append((ch["name"], ch["id"]))
+        except KeyError:
+            print("Authentication failed!\nPlease check your OAuth environment variable.")
+            sys.exit(1)
 
         return ch_list
 
@@ -64,26 +80,31 @@ class OnThisDay:
         """
         list_msgs = []
         for ch in self.channel_list:
-            ch_hist = self.client.api_call(method="channels.history",
-                                           channel=ch[CH_ID],
-                                           count=1000,
-                                           inclusive=True,
-                                           latest=self.time("start", time_frame),
-                                           oldest=self.time("end", time_frame)
-                                           )
-            print("Data for {ch} fetched under {frame} time-frame with {num} messages".format(
-                ch=ch[CH_NAME],
-                frame=time_frame,
-                num=len(ch_hist.get("messages", []))
-            ))
-            if ch_hist is not None:
-                for msg in ch_hist["messages"]:
-                    if msg["type"] == "message":
-                        content = msg["text"]
-                        user = msg["user"]
-                        reacts = msg.get("reactions", 0)
+            for x in range(6):
+                ch_hist = self.client.api_call(method="channels.history",
+                                               channel=ch[CH_ID],
+                                               count=1000,
+                                               inclusive=True,
+                                               latest=self.time("end", time_frame, x),
+                                               oldest=self.time("start", time_frame, x)
+                                               )
+                print("Data for {ch} fetched under {frame} (interation: {iter}) time-frame with {num} messages".format(
+                    ch=ch[CH_NAME],
+                    frame=time_frame,
+                    iter=x,
+                    num=len(ch_hist.get("messages", []))
+                ))
+                if ch_hist is not None:
+                    for msg in ch_hist["messages"]:
+                        if msg["type"] == "message":
+                            content = msg.get("text", "false")
+                            user = msg.get("user", "user detection failed")
+                            reacts = msg.get("reactions", [])
+                            reacts_count = 0
+                            for reaction in reacts:
+                                reacts_count += reaction.get('count', 0)
 
-                        list_msgs.append((content, ch[CH_NAME], user, reacts))
+                            list_msgs.append((content, ch[CH_NAME], user, reacts_count))
 
         return list_msgs
 
@@ -99,11 +120,38 @@ class OnThisDay:
         max_reacts = 0
         msg_index = 0
         for index, msg in enumerate(self.messages_ch_list):
+            # print(msg)
             if msg[MSG_REACTS] > max_reacts:
                 msg_index = index
                 max_reacts = msg[MSG_REACTS]
 
         return msg_index
+
+    def decorate_msg(self):
+        """
+        Using search method of Slack API, returns a better metadata for the message
+
+        :return:
+        decorated_msg
+        type: (content, channel, creator, link)
+        """
+        msg_metadata = self.client.api_call('search.messages',
+                                            query=self.message[MSG_CONTENT],
+                                            )
+        print(msg_metadata)
+        if msg_metadata["ok"]:
+            msg_metadata = msg_metadata["messages"]['matches'][0]
+            decorated_msg = (
+                msg_metadata["text"],
+                msg_metadata["channel"]["name"],
+                msg_metadata["username"],
+                msg_metadata["permalink"]
+            )
+        else:
+            print("message decoration failed!")
+            sys.exit(1)
+
+        return decorated_msg
 
     def post_msg(self):
         """
@@ -113,28 +161,34 @@ class OnThisDay:
         NONE
 
         """
+        channel_id = 'random'
         for ch in self.channel_list:
-            if ch[CH_ID] == self.message[MSG_CH]:
-                channel_name = ch[CH_NAME]
+            if ch[CH_NAME] == 'random':
+                channel_id = ch[CH_ID]
                 break
         msg = "Here's what was trending OnThisDay!\n\n>"\
-              + self.message[MSG_CONTENT]+"\n\n-- by " + \
-              self.message[MSG_CREATOR] + " in #"+channel_name
+              + self.decorated_message[MSG_CONTENT]+"\n\n-- by @" + \
+              self.decorated_message[MSG_CREATOR] + " in #" + \
+              self.decorated_message[MSG_CH] + "\n\n" + \
+              self.decorated_message[MSG_LINK]
         response = self.client.api_call(method="chat.postMessage",
-                                        channel=msg[MSG_CH],
-                                        text=msg
+                                        channel=channel_id,
+                                        text=msg,
+                                        link_names=True,
+                                        username="Janus"
                                         )
         if response["ok"]:
-            print("nostalgia spread!")
+            print("Yay, nostalgia spread!")
         else:
             print("failed to invoke memories!")
 
-    def time(self, type_of, time_frame):
+    def time(self, type_of, time_frame, iter_value):
         """
         converts current date, last year, 23:59 (or 00:01) to timestamp recognised by slack.
 
         :param type_of: whether the start time or end time is required
         :param time_frame: specified if year is to be decreased or month
+        :param iter_value: specifies the amount by which year/month is to be decreased
 
         :return:
         start_time
@@ -142,18 +196,19 @@ class OnThisDay:
 
         """
         if time_frame == "year":
-            year = str(self.now.year - 1)
+            year = str(self.now.year - 1 - iter_value)
             month = self._convert(self.now.month)
         else:
             year = str(self.now.year)
-            month = self._convert(self.now.month - 1)
+            month = self._convert(self.now.month - 1 - iter_value)
         day = self._convert(self.now.day)
         time_str = day+"/"+month+"/"+year
 
         if type_of == "start":
-            time_str = time_str+" 00:00"
+            time_str = time_str+" 00:01"
         else:
             time_str = time_str+" 23:59"
+        # print(time_str)
 
         start_time = time.mktime(datetime.datetime.strptime(time_str, "%d/%m/%Y %H:%M").timetuple())
         return start_time
@@ -174,6 +229,7 @@ class OnThisDay:
             str_num = str(num)
 
         return str_num
+
 
 if __name__ == "__main__":
     OnThisDay()
