29d7e872e0fe0682541c998273cc04f4073bb4e2
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2018-08-23 16:48:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,11 +17,23 @@ class OnThisDay:
     def __init__(self):
 
         self.now = datetime.datetime.now()
+        # print(self.now)
         self.client = slck.SlackClient(os.getenv('SLACK_BOT_TOKEN'))
+        print("Slack bot authenticated!\nrequesting channel list...")
         self.channel_list = self.list_channels()
-        self.messages_ch_list = self.list_messages()
-        self.message = self.messages_ch_list[self.max_emoji_msg()]
-        self.post_msg()
+        print("{num} channels received!\nQuerying channels...".format(
+            num=len(self.channel_list)
+        ))
+        # print(self.channel_list)
+        self.messages_ch_list = self.list_messages("year")
+        if len(self.messages_ch_list) == 0:
+            self.message = self.list_messages("month")
+
+        if len(self.messages_ch_list) > 0:
+            self.message = self.messages_ch_list[self.max_emoji_msg()]
+            self.post_msg()
+        else:
+            print("You guys should be talking more!")
 
     def list_channels(self):
         """
@@ -33,15 +45,17 @@ class OnThisDay:
 
         """
         ch_list = []
-        ch_dict = self.client.api_call(method="https://slack.com/api/channels.list")
+        ch_dict = self.client.api_call(method="channels.list")
         for ch in ch_dict["channels"]:
             ch_list.append((ch["name"], ch["id"]))
 
         return ch_list
 
-    def list_messages(self):
+    def list_messages(self, time_frame):
         """
         lists all the messages for the day of all channels
+
+        :param time_frame: (str) specifies if the data is to be collected for past year or past month
 
         :return:
         msg_list
@@ -50,19 +64,26 @@ class OnThisDay:
         """
         list_msgs = []
         for ch in self.channel_list:
-            ch_hist = self.client.api_call(method="https://slack.com/api/channels.history",
+            ch_hist = self.client.api_call(method="channels.history",
                                            channel=ch[CH_ID],
                                            count=1000,
-                                           latest=self.time("start"),
-                                           oldest=self.time("end")
+                                           inclusive=True,
+                                           latest=self.time("start", time_frame),
+                                           oldest=self.time("end", time_frame)
                                            )
-            for msg in ch_hist["messages"]:
-                if msg["type"] == "message":
-                    content = msg["text"]
-                    user = msg["user"]
-                    reacts = msg.get("reactions", 0)
+            print("Data for {ch} fetched under {frame} time-frame with {num} messages".format(
+                ch=ch[CH_NAME],
+                frame=time_frame,
+                num=len(ch_hist.get("messages", []))
+            ))
+            if ch_hist is not None:
+                for msg in ch_hist["messages"]:
+                    if msg["type"] == "message":
+                        content = msg["text"]
+                        user = msg["user"]
+                        reacts = msg.get("reactions", 0)
 
-                    list_msgs.append((content, ch[CH_NAME], user, reacts))
+                        list_msgs.append((content, ch[CH_NAME], user, reacts))
 
         return list_msgs
 
@@ -99,7 +120,7 @@ class OnThisDay:
         msg = "Here's what was trending OnThisDay!\n\n>"\
               + self.message[MSG_CONTENT]+"\n\n-- by " + \
               self.message[MSG_CREATOR] + " in #"+channel_name
-        response = self.client.api_call(method="https://slack.com/api/chat.postMessage",
+        response = self.client.api_call(method="chat.postMessage",
                                         channel=msg[MSG_CH],
                                         text=msg
                                         )
@@ -108,18 +129,24 @@ class OnThisDay:
         else:
             print("failed to invoke memories!")
 
-    def time(self, type_of):
+    def time(self, type_of, time_frame):
         """
         converts current date, last year, 23:59 (or 00:01) to timestamp recognised by slack.
 
         :param type_of: whether the start time or end time is required
+        :param time_frame: specified if year is to be decreased or month
+
         :return:
         start_time
         type: string (slack timestamp)
 
         """
-        year = str(self.now.year - 1)
-        month = self._convert(self.now.month)
+        if time_frame == "year":
+            year = str(self.now.year - 1)
+            month = self._convert(self.now.month)
+        else:
+            year = str(self.now.year)
+            month = self._convert(self.now.month - 1)
         day = self._convert(self.now.day)
         time_str = day+"/"+month+"/"+year
 
