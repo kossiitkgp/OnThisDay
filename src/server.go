5ab69e46e8c6e19d0589d1e9628c3860077281c1
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2018-10-01 04:57:26 +0530

    Refactor code, improve logging and a few other minor changes

--- src/server.go
+++ src/server.go
@@ -3,48 +3,93 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"time"
 )
 
+const PORT = ":4000"
+const DIR = "SavedCreds"
+
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println(time.Now())
-	WebClient()
+	log.Println("Launching the OAuth Sever on port " + PORT[1:])
+	OAuthServer()
+	log.Fatal("Server shutdown!")
 }
 
-func WebClient() {
-	port := ":8080"
+func OAuthServer() {
+	// using mux instead of direct http handling
 	mux := http.DefaultServeMux
-	fmt.Printf("Starting server on %v", port)
-	mux.HandleFunc("/init", InitHandler)
-	mux.HandleFunc("/oauth/redirect", RedirectHandler)
-	// mux.HandleFunc("/auth", WorldHelloHandler)
-	err := http.ListenAndServe(port, mux)
+	mux.HandleFunc("/init", InitHandler) // A simple button (for testing)
+	log.Println("Init handler setup.")
+	mux.HandleFunc("/redirect", RedirectHandler) // Redirected to code here, IMP
+	log.Println("Redirect handler setup,")
+	log.Println("Listening on the port...")
+	err := http.ListenAndServe(PORT, mux)
 	if err != nil {
-		fmt.Println("Server is down!")
-		fmt.Printf("%+v", err)
+		log.Println("Failed to start the server!")
+		log.Fatal("%+v", err)
 	}
 }
 
-func InitHandler(repwri http.ResponseWriter, req *http.Request) {
-	http.ServeFile(repwri, req, "src/web_root/index.html")
+func InitHandler(w http.ResponseWriter, r *http.Request) {
+	// serves the test webpage to init the process
+	log.Println("InitHandler accessed!")
+	http.ServeFile(w, r, "src/test.html")
 }
 
 func RedirectHandler(w http.ResponseWriter, r *http.Request) {
+	// Handles redirect from the slack server
 
-	uri := "https://slack.com/api/oauth.access?code=" + r.URL.Query().Get("code") + "&client_id=" + os.Getenv("CLIENT_ID") + "&client_secret=" + os.Getenv("CLIENT_SECRET") + "&redirect_uri=" + os.Getenv("REDIRECT_URI")
-	resp, Geterr := http.Get(uri)
-	if Geterr != nil {
-		log.Fatal("Failed to authenticate")
+	log.Println("RedirectHandler accessed!")
+	AuthCode := r.URL.Query().Get("code")
+	uri := "https://slack.com/api/oauth.access?code=" + r.URL.Query().Get("code") +
+		"&client_id=" + os.Getenv("CLIENT_ID") +
+		"&client_secret=" + os.Getenv("CLIENT_SECRET") +
+		"&redirect_uri=" + os.Getenv("REDIRECT_URI")
+
+	log.Println("Following URI generated:\n" + uri)
+
+	GetResp, GetErr := http.Get(uri)
+	if GetErr != nil {
+		log.Println("Failed to authenticate with Slack")
+		log.Fatal("%+v", GetErr)
 	}
+	log.Println("Code received successfully!")
+
 	var JSONResp interface{}
-	jsonDecoder := json.NewDecoder(resp.Body)
+	jsonDecoder := json.NewDecoder(GetResp.Body)
 	JSONerr := jsonDecoder.Decode(&JSONResp)
 	if JSONerr != nil {
 		log.Fatal("Failed to decode the body")
 	}
-	fmt.Printf("%+v", JSONResp)
+	log.Println("authenticated tokens received!")
+	// fmt.Printf("%+v", JSONResp)
+	SaveCred(JSONResp, AuthCode+".temp")
+}
+
+func SaveCred(JSONResp interface{}, FileName string) {
+	CheckDir()
+	path := fmt.Sprint(DIR, FileName)
+	os.Remove(path)
+	log.Println("Creds being saved to " + path)
+
+	ToSave, MarshalErr := json.Marshal(JSONResp)
+	if MarshalErr != nil {
+		log.Println("Failed to save creds")
+		log.Printf("%+v", MarshalErr)
+	}
+	ioutil.WriteFile(path, ToSave, 0644)
+}
+
+func CheckDir() {
+	_, DirErr := os.Stat(DIR)
+	if DirErr != nil {
+		if os.IsNotExist(DirErr) {
+			os.Mkdir(DIR, 0755)
+		} else {
+			log.Println(DirErr)
+		}
+	}
 }
